79201358484bda636104d199d886cbdc13bae340
xani-dev <xani.gn@gmail.com>
2023-05-31 17:26:25 -0400

    Ex1 Finished

--- src/Pages/DrinkPage.js
+++ src/Pages/DrinkPage.js
@@ -5,24 +5,49 @@ import './DrinkPage.scss'
 
 export const DrinkPage = () => {
     return (
-        <div className='DrinkPage'>
-            <div className='Center_Content'>
-                <div className='DrinkPage_Header'>
-                    Drinks
-                </div>
-                <div className='DrinkPage_Content'>
-                    <DrinksTable />
-                </div>
-                <div className='DrinkPage_Content'>
-                    <div className='DrinkPage_Pix'>
-                        <Image width='800px' rounded={true} fluid={true} src='coffee.jpg' alt='coffee' />
-                        <br /><br />
-                        <Image width='800px' rounded={true} fluid={true} src='tea.jpg' alt='tea' />
-                        <br /><br />
-                        <Image width='800px' rounded={true} fluid={true} src='lemonade.jpg' alt='lemonade' />
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+			<div className='DrinkPage'>
+				<div className='Center_Content'>
+					<div className='DrinkPage_Header'>Drinks</div>
+					<div className='DrinkPage_Content'>
+						<DrinksTable />
+					</div>
+					<div className='DrinkPage_Content'>
+						<div className='DrinkPage_Pix'>
+							<Image
+								width='800px'
+								rounded={true}
+								fluid={true}
+								src='milk.jpg'
+								alt='milk'
+							/>
+							<Image
+								width='800px'
+								rounded={true}
+								fluid={true}
+								src='coffee.jpg'
+								alt='coffee'
+							/>
+							<br />
+							<br />
+							<Image
+								width='800px'
+								rounded={true}
+								fluid={true}
+								src='tea.jpg'
+								alt='tea'
+							/>
+							<br />
+							<br />
+							<Image
+								width='800px'
+								rounded={true}
+								fluid={true}
+								src='lemonade.jpg'
+								alt='lemonade'
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
 }
